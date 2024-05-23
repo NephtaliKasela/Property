0e5be01786d4a2c18ba050650ab3db90b02c60c0
Nephtali Kasela <nephtalikasela04@gmail.com>
2024-05-23 16:30:35 +0300

    Add more design

--- Property/Controllers/Admin/AdminController.cs
+++ Property/Controllers/Admin/AdminController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Property.DTOs.Actions;
+using Property.Services.AgentServices;
+using Property.Services.ProductService.ProductServicesRealEstate;
+using Property.Services.UserApplicationServices;
 
 namespace Property.Controllers.Admin
 {
 	public class AdminController : Controller
 	{
-		public IActionResult Index()
+        private readonly IApplicationUserServices _applicationUserServices;
+		private readonly IAgentServices _agentServices;
+		private readonly IProductServicesRealEstate _productServicesRealEstate;
+
+        public AdminController(IApplicationUserServices applicationUserServices, IAgentServices agentServices, IProductServicesRealEstate productServicesRealEstate)
+        {
+            _applicationUserServices = applicationUserServices;
+			_agentServices = agentServices;
+			_productServicesRealEstate = productServicesRealEstate;
+        }
+
+        public async Task<IActionResult> Index()
 		{
-			return View();
+            var products = await _productServicesRealEstate.GetAllProducts();
+            var users = await _applicationUserServices.GetAllUsers();
+            var agents = await _agentServices.GetAllAgents();
+            var v = new AdminDashboard_action();
+
+            v.Products = products.Data;
+            v.Users = users.Data;
+            v.Agents = agents.Data;
+
+			return View(v);
 		}
 	}
 }
---
+++ Property/DTOs/Actions/AdminDashboard_action.cs
@@ -0,0 +1,20 @@
+﻿using Property.DTOs.Agent;
+using Property.DTOs.Product.ProductRealEstate;
+using Property.DTOs.UserApplication;
+
+namespace Property.DTOs.Actions
+{
+    public class AdminDashboard_action
+    {
+        public List<GetProductRealEstateDTO> Products { get; set; }
+        public List<GetApplicationUserDTO> Users { get; set; }
+        public List<GetAgentDTO> Agents { get; set; }
+
+        public AdminDashboard_action() 
+        { 
+            Products = new List<GetProductRealEstateDTO>();
+            Users = new List<GetApplicationUserDTO>();
+            Agents = new List<GetAgentDTO>();
+        }
+    }
+}
